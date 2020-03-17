d497b9ac93850ebdd013e3fe9a23b027f7dc614f
Mastersam <abadasamuelosp@gmail.com>
2020-03-17 09:54:10 -0700

    dummy chats

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,41 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummy = [
+  new ChatModel(
+    name: "Abada Samuel",
+    message: "Hello, what's going on?",
+    time: "15:39",
+    avatarUrl: "",
+  ),
+  new ChatModel(
+    name: "Abada Samuel",
+    message: "Hello, what's going on?",
+    time: "15:39",
+    avatarUrl: "",
+  ),
+  new ChatModel(
+    name: "Abada Samuel",
+    message: "Hello, what's going on?",
+    time: "15:39",
+    avatarUrl: "",
+  ),
+  new ChatModel(
+    name: "Abada Samuel",
+    message: "Hello, what's going on?",
+    time: "15:39",
+    avatarUrl: "",
+  ),
+  new ChatModel(
+    name: "Abada Samuel",
+    message: "Hello, what's going on?",
+    time: "15:39",
+    avatarUrl: "",
+  )
+];
