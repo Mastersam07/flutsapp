12f611fef875f12f29d2d192a00d74ee1bfd8c75
Mastersam <abadasamuelosp@gmail.com>
2020-03-16 22:24:50 -0700

    Initial commit

---
+++ lib/screens/whatsapp_home.dart
@@ -0,0 +1,55 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class WhatsAppHome extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  WhatsAppHome({this.cameras});
+
+  @override
+  _WhatsAppHomeState createState() => _WhatsAppHomeState();
+}
+
+class _WhatsAppHomeState extends State<WhatsAppHome>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  bool showFab = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(vsync: this, initialIndex: 1, length: 4);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WhatsApp"),
+        elevation: 0.7,
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.camera_alt)),
+            Tab(text: "CHATS"),
+            Tab(
+              text: "STATUS",
+            ),
+            Tab(
+              text: "CALLS",
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          Icon(Icons.search),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+          ),
+          Icon(Icons.more_vert)
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
