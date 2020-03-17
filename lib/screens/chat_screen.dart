6305a58ec6b7d79aa7b22d3bd5ef451f0e056d68
Mastersam <abadasamuelosp@gmail.com>
2020-03-17 09:53:50 -0700

    demo chats

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import '../models/chat_model.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: Text('Chat Screen'),
+    return ListView.builder(
+      itemCount: dummy.length,
+      itemBuilder: (context, i) => new Column(
+        children: <Widget>[
+          new Divider(
+            height: 10.0,
+          ),
+          new ListTile(
+            leading: new CircleAvatar(
+              foregroundColor: Theme.of(context).primaryColor,
+              backgroundColor: Colors.grey,
+            ),
+            title: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new Text(
+                  dummy[i].name,
+                  style: new TextStyle(fontWeight: FontWeight.bold),
+                ),
+                new Text(
+                  dummy[i].time,
+                  style: new TextStyle(color: Colors.grey, fontSize: 14.0),
+                ),
+              ],
+            ),
+            subtitle: new Container(
+              padding: EdgeInsets.only(top: 5.0),
+              child: new Text(
+                dummy[i].message,
+                style: new TextStyle(color: Colors.grey, fontSize: 15.0),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
